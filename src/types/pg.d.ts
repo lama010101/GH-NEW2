2f4b8384ae27b49432c17b08972df4c0c6836404
lama010101 <laurent.martenot+github@gmail.com>
2026-04-21 14:44:12 +0700

    Fix issue #4: pg shim + disabled server loop scripts

--- src/types/pg.d.ts
+++ src/types/pg.d.ts
@@ -14,9 +14,15 @@ declare module "pg" {
     };
   }
 
+  export interface PoolClient {
+    query<R extends QueryResultRow = QueryResultRow>(text: string, values?: unknown[]): Promise<QueryResult<R>>;
+    release(): void;
+  }
+
   export class Pool {
     constructor(config?: PoolConfig);
     query<R extends QueryResultRow = QueryResultRow>(text: string, values?: unknown[]): Promise<QueryResult<R>>;
+    connect(): Promise<PoolClient>;
     end(): Promise<void>;
   }
 }
