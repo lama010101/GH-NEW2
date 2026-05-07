7e8c561099fdb06e6660773e7684a1d6554185c8
lama010101 <laurent.martenot+github@gmail.com>
2026-05-07 20:35:11 +0700

    vercel fix

--- .next/types/app/compete/page.ts
+++ .next/types/app/compete/page.ts
@@ -1,8 +1,8 @@
-// File: D:\GH-NEW\src\app\page.tsx
-import * as entry from '../../../src/app/page.js'
+// File: D:\GH-NEW\src\app\compete\page.tsx
+import * as entry from '../../../../src/app/compete/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../src/app/page.js')
+type TEntry = typeof import('../../../../src/app/compete/page.js')
 
 // Check that the entry is a valid entry
 checkFields<Diff<{
